882928bda687f1924453c7b32191127f52e33c62
xirishi0 <86041651+xirishi0@users.noreply.github.com>
2021-07-07 10:02:52 +0800

    MakeCode 项目的初始文件

---
+++ mygame.ts
@@ -0,0 +1,24 @@
+namespace SpriteKind {
+    export const p1atk = SpriteKind.create()
+    export const p2atk = SpriteKind.create()
+    export const p1body = SpriteKind.create()
+    export const p2body = SpriteKind.create()
+}
+//%icon="\uf007" color="#6A6FEA"
+namespace 人物{} //"\uf183" "\uf113"
+//%icon="\uf132" color="#B6392F"
+namespace 技能{}//"\uf132" "\uf198" "\uf140" 
+//%icon="\uf135" color="#458FAA"
+namespace 弹射物{} //"#BCE1F0"
+//%icon="\uf008" color="#BCE190"
+namespace 动画{}//"\uf008" "\uf152" 
+//"\uf087"
+//"\uf197"
+namespace myGame{
+    //%block
+    //%blockNamespace=人物 
+    //%blockId=f block="fff"
+    export function f(){
+
+    }
+}
